e283ab1217cca6c9fa505f105bbfb35751b7e91b
Nath-Ramirez <rhnathaly2@gmail.com>
2026-04-24 23:14:09 -0500

    New Rallet and new DB

--- src/expense.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Expense {
-    pub amount: i64,
-    pub category: String,
-    pub date: String,
-    pub note: Option<String>,
-}
